e526046f23ad24bbfa4e5b498dd2c75489dc390b
jankes97 <35306040+jankes97@users.noreply.github.com>
2019-01-12 11:27:13 +0100

    Update day19.c

--- adevnt-code/day19.c
+++ adevnt-code/day19.c
@@ -1,14 +1,4 @@
 /*
-Pierwsza linia (#ip 0) wskazuje, że wskaźnik instrukcji powinien być powiązany z rejestracją 0 w tym programie. Nie jest to instrukcja, więc wartość wskaźnika instrukcji nie zmienia się podczas przetwarzania tej linii.
-    Wskaźnik instrukcji zawiera 0, dlatego wykonywana jest pierwsza instrukcja (seti 5 0 1). Aktualizuje rejestr 0 do bieżącej wartości wskaźnika instrukcji (0), ustawia rejestr 1 na 5, ustawia wskaźnik instrukcji na wartość rejestru 0 (co nie ma skutku, ponieważ instrukcja nie modyfikowała rejestru 0), a następnie dodaje jeden do wskaźnika instrukcji.
-    Wskaźnik instrukcji zawiera 1, więc wykonywana jest druga instrukcja, seti 6 0 2. Jest to bardzo podobne do instrukcji przed nim: 6 jest przechowywane w rejestrze 2, a wskaźnik instrukcji pozostaje z wartością 2.
-    Wskaźnik instrukcji wynosi 2, co wskazuje na instrukcję addi 0 1 0. Jest to podobne do skoku względnego: wartość wskaźnika instrukcji 2 jest ładowana do rejestru 0. Następnie addi znajduje wynik dodania wartości do rejestru 0 i wartość 1, przechowująca wynik, 3, z powrotem w rejestrze 0. Register 0 jest następnie kopiowany z powrotem do wskaźnika instrukcji, co spowoduje, że skończy 1 większy niż powinien i pominąć następną instrukcję (addr 1 2 3) całkowicie. Na końcu 1 jest dodawany do wskaźnika instrukcji.
-    Wskaźnik instrukcji wynosi 4, więc uruchamiana jest instrukcja setr 1 0 0. Jest to jak skok bezwzględny: kopiuje wartość zawartą w rejestrze 1, 5, do rejestru 0, co powoduje, że kończy się w wskaźniku instrukcji. Wskaźnik instrukcji jest następnie inkrementowany, pozostawiając go na poziomie 6.
-    Wskaźnik instrukcji wynosi 6, więc instrukcja seti 9 0 5 przechowuje 9 w rejestrze 5. Wskaźnik instrukcji jest inkrementowany, co powoduje, że wskazuje on program, a więc program się kończy.
-
-Jaka wartość pozostaje w rejestrze 0, gdy proces w tle zatrzymuje się?
-
-
 #ip 3
 addi 3 16 3   r3=r3+16
 seti 1 6 5	  r1=1 (pierwsza zostaje, druga usun) 
