e4eec74738bd8aeeb80bb95445372c6ec2f501f6
jankes97 <35306040+jankes97@users.noreply.github.com>
2019-01-18 08:33:04 +0100

    Update TheFeastofManyBeasts.c

--- codewars/TheFeastofManyBeasts.c
+++ codewars/TheFeastofManyBeasts.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool feast(const char *beast, const char *dish)
-{
-  return beast[0] == dish[0] && beast[strlen(beast) - 1] == dish[strlen(dish) - 1] ? true : false;
+bool feast(const char* beast, const char* dish) {
+
+    // <---- hajime!
+    if(beast[0] != dish[0])
+    {
+      return false;
+    }
+    
+    if(beast[strlen(beast)-1] != dish[strlen(dish)-1])
+    {
+      return false;
+    }
+    return true;
 }
