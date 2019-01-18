2a1c40744437e3c249609d8f2ae31a12ae8d452e
jankes97 <35306040+jankes97@users.noreply.github.com>
2019-01-18 08:43:12 +0100

    Update L1-Set_alarm.c

--- codewars/L1-Set_alarm.c
+++ codewars/L1-Set_alarm.c
@@ -1,16 +1,19 @@
-bool set_alarm(bool employed, bool vacation) {
-
-    if (employed == true && vacation == true){
-      return false;
-}
-
-    if (employed == false && vacation == true){
-      return false;
-}
-if (employed == false && vacation == false){
-      return false;
-}
-if (employed == true && vacation == false){
-      return true;
-}
+bool set_alarm(bool employed, bool vacation) 
+{
+  if (employed == true && vacation == true)
+  {
+    return false;
+  }
+  if (employed == false && vacation == true)
+  {
+    return false;
+  }
+  if (employed == false && vacation == false)
+  {
+    return false;
+  }
+  if (employed == true && vacation == false)
+  {
+    return true;
+  }
 }
