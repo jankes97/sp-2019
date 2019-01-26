1024d98379b207e904436d6544757c51508efcf7
jankes97 <35306040+jankes97@users.noreply.github.com>
2019-01-26 08:29:54 +0100

    Update day3.c

--- adevnt-code/day3/day3.c
+++ adevnt-code/day3/day3.c
@@ -28,13 +28,12 @@ int main(){
 
 void load_data(int claimLen, int width, int height)
 {
-  FILE *fp;
+  FILE *fp = fopen(INPUT, "r");
 
   char *line = NULL;
   size_t len = 0;
   Claim *claims = malloc(sizeof(Claim));
-
-  fp = fopen(INPUT, "r");
+  
   while (getline(&line, &len, fp) != -1){
     line[strcspn(line, "\r\n")] = 0;
     Claim claim = {atoi(line + 1), atoi(line + advanceTo(line, '@') + 1), atoi(line + advanceTo(line, ',') + 1), atoi(line + advanceTo(line, ':') + 2), atoi(line + advanceTo(line, 'x') + 1)};
